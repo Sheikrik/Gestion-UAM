ddc0fe925480f3aede6c8bb3d7661db821966d34
Mauricio Del Castillo Morales <sheikrik@gmail.com>
2017-07-12 14:53:58 -0500

    Unificacion
    La clase operaciones se hace concreta y se unifican los metodos eliminar, registrar y buscar

--- ProyectoFinal/src/operaciones/Operaciones.java
+++ ProyectoFinal/src/operaciones/Operaciones.java
@@ -1,53 +1,55 @@
 package operaciones;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.Iterator;
-import java.util.LinkedList;
 import java.util.List;
-import modeloPersona.Persona;
 
-public abstract class Operaciones<E> {
-/*	protected List<E> lista = new LinkedList<E>();//mau altere esta linea ya que no me deja hacer el registro de nada que por que "no se aplica al argumento " 
-	                                              //entonces lo voy a comentar y y abajp voy a poner el "nuevo vale (L)
-	public void registrar(E elemento){
-		lista.add(elemento);
-		System.out.println("Elemento registrado");
-	}*/
-	protected List<E> lista = new LinkedList<E>();	
+public class Operaciones<E> {
 	
 	public void registrar(List<E> lista,E elemento){
 		lista.add(elemento);
 		System.out.println("Elemento registrado");
 	}
 	
-	public void eliminarNombre(String nombre){
+	public void eliminar(List<E> lista,String nomClase,String nomMetodo,String comparador) throws ClassNotFoundException, NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException{
 		Iterator<E> iterador = lista.iterator();
+		Method metodo = pasoMetodo(nomClase,nomMetodo);
+		boolean estado = false;
 		
-		while (iterador.hasNext()){
+		while (iterador.hasNext() || estado==true){
 			E e = iterador.next();
-			if(((Persona) e).getNombre().equalsIgnoreCase(nombre)){
+			String parametro = (String) metodo.invoke(e,null);
+			
+			if(parametro.equalsIgnoreCase(comparador)){
 				lista.remove(e);
-				System.out.println("Se ha eliminado a " + ((Persona) e).getNombre());
-				break;
+				System.out.println("Se ha eliminado a " + parametro);
+				estado = true;
 			}
 		}
 	}
 	
-	public Persona busquedaNombre(String parametro){
-		Persona elemento = null;
+	public E busqueda(List<E> lista,String nomClase,String nomMetodo,String comparador) throws ClassNotFoundException, NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException{
 		Iterator<E> iterador = lista.iterator();
+		Method metodo = pasoMetodo(nomClase,nomMetodo);
+		E elemento = null;
 		
-		while (iterador.hasNext())
-		{
+		while (iterador.hasNext()){
 			E e = iterador.next();
-			if(((Persona) e).getNombre().equalsIgnoreCase(parametro))
-			{
-				System.out.println("Persona encontrada " + ((Persona) e).getNombre());
-				elemento = (Persona) e;
+			String parametro = (String) metodo.invoke(e,null);
+			
+			if(parametro.equalsIgnoreCase(comparador)){
+				System.out.println("Persona encontrada " +  parametro);
+				elemento = e;
 			}
 		}
 		return elemento;
 	}
 	
-	public abstract void imprimeLista();	
-	public abstract void actualizacion(String parametro);
+	private Method pasoMetodo(String clase,String metodo) throws ClassNotFoundException, NoSuchMethodException, SecurityException{
+		Class nomClase = Class.forName(clase);
+		Method nomMetodo = nomClase.getMethod(metodo, null);
+		
+		return nomMetodo;
+	}
 }
